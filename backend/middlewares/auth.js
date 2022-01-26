0b52377efe45364afdaf1c20c36f6ced6f511c37
Svetlana Zh <lady_zhivtsova@gmail.com>
2022-01-26 16:58:45 +0300

    dev-secret

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -21,7 +21,7 @@ module.exports = (req, res, next) => {
   const token = authorization.replace('Bearer ', '');
   let payload;
   try {
-    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'prod-secret');
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
   } catch (err) {
     return handleAuthError(err);
   }
